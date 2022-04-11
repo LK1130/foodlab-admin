b33cdfbaa1528a75ca7ef51458b7f88f7a0e4855
Aung Min Khant <aungmk299@gmail.com>
2022-04-11 17:18:07 +0630

    batch file

---
+++ food_lab(admin)/photo_copy.bat
@@ -0,0 +1,4 @@
+@echo off 
+xcopy E:\food_lab(admin)\food_lab(admin)\storage\app\public\ProductImage\*.* E:\food_lab(customer)\food_lab(customer)\storage\app\public\ProductImage\. /Y
+
+echo "The program has completed"
